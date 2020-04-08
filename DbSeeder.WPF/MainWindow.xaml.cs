38335fa02adf4df5000d292eb217dc8681573c97
AndrasHorinka <horinka.andras@gmail.com>
2020-04-08 18:40:06 +0200

    WPF init

---
+++ DbSeeder.WPF/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+
+namespace DbSeeder.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception e)
+            {
+            }
+
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            var psi = new ProcessStartInfo
+            {
+                FileName = "https://github.com/AndrasHorinka/DbSeeder",
+                UseShellExecute = true
+            };
+            Process.Start(psi);
+
+        }
+    }
+}
