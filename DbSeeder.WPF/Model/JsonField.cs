8d6c96bfb6226ad11c11de06e698884ce750c60e
AndrasHorinka <horinka.andras@gmail.com>
2020-04-17 08:19:16 +0200

    AddField extended / content stored into new class (JsonField) Check if all fields are filled

---
+++ DbSeeder.WPF/Model/JsonField.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DbSeeder.WPF.Model
+{
+    public class JsonField<T>
+    {
+        public string FieldName { get; set; }
+        public T FieldValue { get; set; }
+
+        public JsonField(string fieldName, T fieldValue)
+        {
+            FieldName = fieldName;
+            FieldValue = fieldValue;
+        }
+
+    }
+}
